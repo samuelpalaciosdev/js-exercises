43a4be7a3f6c07b9043261fba8807d48fe814352
Samuel Palacios <samuelpl0888@gmail.com>
2022-06-20 12:02:37 -0400

    11 Remove vowels with for loop

--- script.js
+++ script.js
@@ -1,44 +1,15 @@
-/*
-Let's go back to Mark and John comparing their BMIs! This time, let's use objects to 
-implement the calculations! Remember: BMI = mass / height ** 2 = mass 
-/ (height * height) (mass in kg and height in meter)
-Your tasks:
-1. For each of them, create an object with properties for their full name, mass, and 
-height (Mark Miller and John Smith)
-2. Create a 'calcBMI' method on each object to calculate the BMI (the same 
-method on both objects). Store the BMI value to a property, and also return it 
-from the method
-3. Log to the console who has the higher BMI, together with the full name and the 
-respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
-Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m 
-tall.
-*/
+const rapid = (string) => {
+  let consonants = [];
+  string = string.toLowerCase();
 
-const mark = {
-  fullName: 'Mark Miller',
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    this.bmi = (this.mass / this.height ** 2).toFixed(2);
-    return this.bmi;
-  },
+  for (let i = 0; i < string.length; i++) {
+    if (['a', 'e', 'i', 'o', 'u'].includes(string[i]) === false) {
+      consonants.push(string[i].toUpperCase());
+    }
+  }
+
+  return consonants.join('');
 };
-
-const john = {
-  fullName: 'John Smith',
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.bmi = (this.mass / this.height ** 2).toFixed(2);
-    return this.bmi;
-  },
-};
-
-mark.calcBMI();
-john.calcBMI();
-
-if (mark.bmi > john.bmi) {
-  console.log(`Mark's BMI (${mark.bmi}) is higher than John's (${john.bmi})`); // Mark's BMI (27.31) is higher than John's (24.19)
-} else if (john.bmi > mark.bmi) {
-  console.log(`John's BMI (${john.bmi}) is higher than Mike's (${mike.bmi})`);
-}
+// Work above this line; do not change code below
+let str = 'John';
+console.log(rapid(str));
