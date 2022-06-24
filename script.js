fa46b4d7662ce743566380c62170546163f4318b
Samuel Palacios <samuelpl0888@gmail.com>
2022-06-24 16:16:23 -0400

    Sum values of 2 inputs

--- script.js
+++ script.js
@@ -1,49 +1,10 @@
-var person = {
-  name: 'John', //String
-  lastname: 'Doe',
-  age: 35, //Number
-  gender: 'male',
-  lucky_numbers: [7, 11, 13, 17], //Array
-  significant_other: person2, //Object, yes the same variable/object defined after
+// adding the function to the window makes it globally available
+window.calculateSumListener = function() {
+	//Return the value of the input #firstNumber
+	var stringA = parseInt(document.getElementById("firstNumber").value);
+	//Return the value of the input #secondNumber
+	var stringB = parseInt(document.getElementById("secondNumber").value);
+	let sum = stringA + stringB;
+	document.querySelector('#resultNumber').value = sum;
+	//your code goes here
 };
-
-var person2 = {
-  name: 'Jane',
-  lastname: 'Doe',
-  age: 38,
-  gender: 'female',
-  lucky_numbers: [2, 4, 6, 8],
-  significant_other: person,
-};
-
-var person3 = {
-  name: 'Jimmy',
-  lastname: 'Doe',
-  age: 13,
-  gender: 'male',
-  lucky_numbers: [1, 2, 3, 4],
-  significant_other: null,
-};
-
-var family = {
-  lastname: 'Doe',
-  members: [person, person2, person3], //Array of objects, don't forget to add Jimmy
-};
-
-function addAllFamilyLuckyNumbers(anArray) {
-  var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
-  for (let i = 0; i < anArray.length; i++) {
-    for (let j = 0; j < anArray[i].lucky_numbers.length; j++) {
-      sumOfAllLuckyNumbers += anArray[i].lucky_numbers[j];
-    }
-  }
-  //To-Do: loop and add; consider nested loops
-  //Hint: use the anArray variable to get all of the lucky numbers
-
-  return sumOfAllLuckyNumbers;
-}
-
-person.lucky_numbers[3] = 33;
-
-//Do not make changes below:
-console.log(addAllFamilyLuckyNumbers(family.members));
